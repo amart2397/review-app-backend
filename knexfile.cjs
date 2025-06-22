0f76f030283a76b25db6c0035f8b6792c8dd5bd1
alex martinez <alexdmartinez.1997@gmail.com>
2025-06-22 18:11:35 -0500

    Add initial database connection and migrations for users, media, and posts tables

---
+++ knexfile.cjs
@@ -0,0 +1,30 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+require('dotenv').config();
+
+console.log('loaded knexfile');
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
