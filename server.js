8a8741ea09a3ff7835616ef84de3cb674f418ca8
alex martinez <alexdmartinez.1997@gmail.com>
2025-06-22 21:50:04 -0500

    Add logger middleware to log all requests

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 import express from "express";
 import router from "./routes/root.js";
 import path from "path";
+import logger from "./middleware/logger.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const __dirname = import.meta.dirname;
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(logger);
 
-app.use('/', router);
+app.use(express.json());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", router);
 
 app.all(/.*/, (req, res) => {
-    res.status(404);
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    }else if(req.accepts('json')){
-        res.json({ message: '404 Not Found'});
-    }else{
-        res.type('txt').send('404 Not Found');
-    }
-})
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
