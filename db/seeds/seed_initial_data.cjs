9f074ee7362b4c4419a2a4d3e727fd62c02676a0
alex martinez <alexdmartinez.1997@gmail.com>
2025-07-08 20:40:46 -0500

    Add reviews data seeding

--- db/seeds/seed_initial_data.cjs
+++ db/seeds/seed_initial_data.cjs
@@ -4,29 +4,6 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex("users").del();
-  await knex("users").insert([
-    {
-      email: "admin@test.com",
-      first_name: "Alex",
-      last_name: "Admin",
-      password: "test1234",
-      role: "admin",
-    },
-    {
-      email: "poster@test.com",
-      first_name: "Alex",
-      last_name: "Poster",
-      password: "test1234",
-      role: "poster",
-    },
-    {
-      email: "user@test.com",
-      first_name: "Alex",
-      last_name: "User",
-      password: "test1234",
-    },
-  ]);
   await knex("media").del();
   await knex("media").insert([
     {
@@ -34,14 +11,49 @@ exports.seed = async function (knex) {
       media_key: "book_key",
       media_title: "Book Title",
       media_description: "Book Description",
-      media_art: "Book Art",
+      media_art: "https://picsum.photos/200/300",
     },
     {
       media_type: "movie",
       media_key: "movie_key",
       media_title: "Movie Title",
       media_description: "Movie Description",
-      media_art: "Movie Art",
+      media_art: "https://picsum.photos/200/300",
+    },
+  ]);
+  await knex("reviews").del();
+  await knex("reviews").insert([
+    {
+      user_id: 1,
+      media_id: 1,
+      review_title: "Admin Review Title One",
+      review_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      review_rating: 2.5,
+    },
+    {
+      user_id: 1,
+      media_id: 2,
+      review_title: "Admin Review Title Two",
+      review_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      review_rating: 5.5,
+    },
+    {
+      user_id: 2,
+      media_id: 1,
+      review_title: "Poster Review Title One",
+      review_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      review_rating: 6.5,
+    },
+    {
+      user_id: 2,
+      media_id: 2,
+      review_title: "Poster Review Title Two",
+      review_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      review_rating: 9.4,
     },
   ]);
 };
